b914a34c2bb41b090a86bd609cb5d6dcc2d8ca1f
JARED-PC\Jared <jstephen2014@fau.edu>
2018-05-08 08:19:54 -0400

    Add viewpoint position/rotation reporting to grabber

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,6 +1,10 @@
 // Copright Jared Selling 2018
 
 #include "Grabber.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+
+#define OUT
 
 
 // Sets default values for this component's properties
@@ -29,6 +33,25 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	//Get player viewpoint this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Position: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString()
+	);
+
+
+	
+
+	//ray-cast out to reach distance
+
+	//see what we hit
 }
 
